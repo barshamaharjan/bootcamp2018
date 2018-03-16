4a93cb94db134ac7fe77c253c37a894e0ca6bf13
Lakwena <30572512+Lakwena@users.noreply.github.com>
2018-03-15 21:45:04 -0400

    changes

--- js/main.js
+++ js/main.js
@@ -29,12 +29,12 @@ $(document).ready(function(){
 			triggerHook: 0.9
 		})
 		.setClassToggle(this, 'fade-in') // add class to project01
-		.addIndicators({
-			name: 'fade scene',
-			colorTrigger: 'black',
-			colorStart: '#75C695',
-			colorEnd: 'pink'
-		}) // this requires a plugin
+		// .addIndicators({
+		// 	name: 'fade scene',
+		// 	colorTrigger: 'black',
+		// 	colorStart: '#75C695',
+		// 	colorEnd: 'pink'
+		// }) 
 		.addTo(controller);
 
 	});
